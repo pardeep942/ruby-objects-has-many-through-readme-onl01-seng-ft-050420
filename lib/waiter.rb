dc05fe22700650bd503b6fdf09ac98d1c42d7be9
PARDEEP KAUR <pkaur84@aol.com>
2020-05-27 15:38:46 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,20 +1,17 @@
-class waiter
+class Waiter
  
-  attr_accessor :waiter, :customer, :total, :tip
+  attr_accessor :name, :yrs_experience
  
   @@all = []
  
-  def initialize(waiter, customer, total, tip=0)
-    @waiter = waiter
-    @customer = customer
-    @total = total
-    @tip = tip
+  def initialize(name, yrs_experience)
+    @name = name
+    @yrs_experience = yrs_experience
     @@all << self
   end
  
   def self.all
     @@all
-end
   end
-  
-  
+ 
+end
