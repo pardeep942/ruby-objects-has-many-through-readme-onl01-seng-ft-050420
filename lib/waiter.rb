fde213541d217e82a075a7eaeae7dd88a946e6d7
PARDEEP KAUR <pkaur84@aol.com>
2020-05-27 15:19:18 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,18 @@
 class Waiter
+  attr_accessor :name, :
+  
+  @@all = [ ]
+  
+  def intialize (name, age)
+    @name = name
+    @age = age
+    @all << self
+  
+end
+ def self.all
+   @@all
+ end
+ 
+end
 
 end
