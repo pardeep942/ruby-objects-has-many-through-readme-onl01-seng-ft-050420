ca8d60c66618c84dd480d0e2f5bf49cf0f92df64
PARDEEP KAUR <pkaur84@aol.com>
2020-05-27 15:18:22 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,16 @@
 class Customer
+  attr_accessor :name, :age
   
+  @@all = [ ]
+  
+  def intialize (name, age)
+    @name = name
+    @age = age
+    @all << self
+  
+end
+ def self.all
+   @@all
+ end
+ 
 end
