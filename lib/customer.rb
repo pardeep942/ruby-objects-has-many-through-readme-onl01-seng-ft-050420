a1108ade47d8bb4c94da7f5fac8180c26c9fd047
PARDEEP KAUR <pkaur84@aol.com>
2020-05-27 15:53:10 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,22 +1,31 @@
 class Customer
-  attr_accessor :name, :age 
 
+ attr_accessor :name, :age
 
-  @@all=[]
+  @@all = []
 
-  def initialize (name, age)
-    @name=name
-    @age=age
-    @@all << self 
+  def initialize(name, age)
+    @name = name 
+    @age = age
+    @@all.push(self)
+  end
+
+  def self.all 
+    @@all
   end 
 
-  def self.all
-    @@all 
+  def new_meal(waiter, total, tip=0)
+    Meal.new(waiter, self, total, tip)
   end 
 
-  def new_meal (waiter,total,tip=0)
-    Meal.new(waiter,self,total,tip)
-  end 
+  def meals
+    Meal.all.select do |meal|
+      meal.customer == self
+    end 
+  end
 
-  end 
-end 	
+  def waiters
+    meals.map do |meal|
+      meal.waiter
+    end 
+  end
